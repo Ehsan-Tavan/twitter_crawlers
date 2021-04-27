b873f83081b9035884839c1cb1c81c0679adc60d
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-04-27 20:44:04 +0430

    add filter_tweets to import directly from package

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .validation import is_header_exist
-from .helper import calculate_date, is_item_exist
+from .helper import calculate_date, is_item_exist, filter_tweets
 from .extract_key_words import extract_key_words
 
