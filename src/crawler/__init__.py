8f33ac1216291632128127dad5a23cae780d9b9c
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-04-27 20:42:37 +0430

    remove filter_tweets

--- src/crawler/__init__.py
+++ src/crawler/__init__.py
@@ -1,2 +1,2 @@
-from .helper import extract_tweets, extract_users_id, get_username, save_data, filter_tweets
+from .helper import extract_tweets, extract_users_id, get_username, save_data
 from .crawl_by_key_words import CrawlKeyWords
