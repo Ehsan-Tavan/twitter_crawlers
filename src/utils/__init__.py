b41772f98404e61c9fa7823723c5184044507515
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-04-27 20:26:05 +0430

    change is_exist to is_item_exist

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .validation import is_header_exist
-from .helper import calculate_date, is_exist
+from .helper import calculate_date, is_item_exist
 from .extract_key_words import extract_key_words
 
