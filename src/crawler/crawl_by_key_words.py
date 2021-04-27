5249929ee5b5562b34276a4d607cee7e8b1a815c
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-04-27 15:42:26 +0430

    add CrawlKeyWords class

---
+++ src/crawler/crawl_by_key_words.py
@@ -0,0 +1,61 @@
+from crawler import extract_tweets, get_username, save_data
+import tweepy as tw
+from utils import calculate_date
+from configuration import get_config
+import copy
+
+
+ARGS = get_config()
+
+
+class CrawlKeyWords:
+    def __init__(self, args):
+        self.args = args
+        self.user_names = list()
+
+    def set_twitter_api(self):
+        """
+
+        :return:
+        """
+        auth = tw.OAuthHandler(self.args.consumer_key, self.args.consumer_secret)
+        auth.set_access_token(self.args.access_token, self.args.access_token_secret)
+        api = tw.API(auth, wait_on_rate_limit=True)
+        return api
+
+    def crawl_key_word(self, key_word, api, start_date, end_date):
+        """
+
+        :param key_word:
+        :param api:
+        :param start_date:
+        :param end_date:
+        :return:
+        """
+        tweets = tw.Cursor(api.search,
+                           q=key_word,  # key word
+                           lang="fa",  # just farsi tweets should crawl
+                           since=start_date,
+                           until=end_date).items(self.args.num_tweets)
+        return tweets
+
+    def work_flow(self, key_words):
+        twitter_api = self.set_twitter_api()
+        start_date, end_date = calculate_date(days=self.args.days)
+        data = [self.crawl_key_word(key_word, twitter_api, start_date, end_date) for key_word in key_words]
+        data2 = copy.deepcopy(data)
+        if self.args.extract_text:
+            tweets = [extract_tweets(key_word_data) for key_word_data in data]
+            if self.args.save_text:
+                save_data(tweets, key_words, self.args.root_dir, start_date, end_date, data_name="tweets")
+
+        if self.args.extract_users:
+            user_names = [get_username(key_word_data) for key_word_data in data2]
+            self.user_names = user_names
+            if self.args.save_users:
+                save_data(user_names, key_words, self.args.root_dir, start_date, end_date, data_name="users")
+
+
+if __name__ == '__main__':
+    MCLASS = CrawlKeyWords(ARGS)
+    MCLASS.work_flow(["بورس", "اقتصاد"])
