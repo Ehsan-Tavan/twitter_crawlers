40576b9c0805150ce0d7fdf0344cced1e37e85f5
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-04-28 10:16:18 +0430

    change crawl_data to crawl_data_dir

--- src/crawler/crawl_by_key_words.py
+++ src/crawler/crawl_by_key_words.py
@@ -48,12 +48,12 @@ class CrawlKeyWords:
         if self.args.extract_text:
             tweets = [extract_tweets(key_word_data) for key_word_data in data]
             if self.args.save_text:
-                save_data(tweets, key_words, self.args.data_dir, self.args.crawl_dir,
+                save_data(tweets, key_words, self.args.data_dir, self.args.crawl_data_dir,
                           start_date, end_date, data_name="tweets")
 
         if self.args.extract_users:
             user_names = [get_username(key_word_data) for key_word_data in data2]
             self.user_names = user_names
             if self.args.save_users:
-                save_data(user_names, key_words, self.args.data_dir, self.args.crawl_dir,
+                save_data(user_names, key_words, self.args.data_dir, self.args.crawl_data_dir,
                           start_date, end_date, data_name="users")
