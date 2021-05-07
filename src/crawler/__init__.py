962732755b6a01bda4b2d8cfb8d32de12fa46d43
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-05-07 20:45:49 +0430

    add load_saved_users to import directly from package

--- src/crawler/__init__.py
+++ src/crawler/__init__.py
@@ -1,3 +1,4 @@
-from .helper import extract_tweets, extract_users_id, get_username, save_data
+from .helper import extract_tweets, extract_users_id, get_username,\
+    save_data, load_saved_users
 from .crawl_by_key_words import CrawlKeyWords
 from .crawl_by_user import CrawlUserTweets
