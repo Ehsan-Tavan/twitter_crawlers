e8753f5c21d31c80a9b098d6295da0c23bb80559
Ehsan-Tavan <tavan.ehsan@gmail.com>
2021-05-07 20:50:09 +0430

    use load_saved_users to do not re-crawl users whose tweets have already been crawled

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 from data_loader import load_data
 from configuration import get_config
-from crawler import CrawlKeyWords, CrawlUserTweets
+from crawler import CrawlKeyWords, CrawlUserTweets, load_saved_users
 from utils import extract_key_words, filter_tweets, save_tweets
 
 ARGS = get_config()
@@ -18,15 +18,18 @@ def main():
     filtered_data = load_data(os.path.join(ARGS.data_dir, ARGS.filter_key_words_path))
     filtered_key_words = extract_key_words(filtered_data, ARGS.filter_key_words_header)
 
+    crawled_users = load_saved_users(ARGS)
+
     filtered_tweets = list()
     for users in user_names:
         for user in users:
-            try:
-                crawl_user_tweets.work_flow(user)
-                tweets = crawl_user_tweets.crawled_data["tweet"]
-                filtered_tweets.append(filter_tweets(tweets, filtered_key_words))
-            except Exception as e:
-                print(e)
+            if user not in crawled_users:
+                try:
+                    crawl_user_tweets.work_flow(user)
+                    tweets = crawl_user_tweets.crawled_data["tweet"]
+                    filtered_tweets.append(filter_tweets(tweets, filtered_key_words))
+                except Exception as e:
+                    print(e)
 
     save_tweets(filtered_tweets, os.path.join(ARGS.data_dir, ARGS.final_tweets_path))
 
